6199384abf3d0cfbe3eede2faea7ac145de935c2
Devansh kukreja <117558669+devansh1401@users.noreply.github.com>
2024-07-31 04:35:26 +0000

    ques 3

---
+++ Assignment 1/q3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 100
+
+void reverse_string(char *str) {
+    int length = strlen(str);
+    int i, j;
+    char temp;
+
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main() {
+    char str[MAX_LENGTH];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = 0;
+
+    reverse_string(str);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
